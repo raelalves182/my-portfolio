598ced2b90e54feb7121517892c9502a6d8128cb
raelalves182 <ysrael.2009@hotmail.com>
2021-11-24 22:39:34 -0300

    feat(header): add routes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className={styles.header}>
+      <nav className="container">
+        <Link to="/">Home</Link>
+        <Link to="/portfolio">Portfolio</Link>
+        <Link to="/contact">Contact me</Link>
+      </nav>
     </div>
   )
 }
 
-export default Header
+export default Header;
