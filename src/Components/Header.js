c61ebcf43ebcab648c3cb078945c9ea34590af5a
raelalves182 <ysrael.2009@hotmail.com>
2021-12-03 15:47:55 -0300

    feat(header): ajustes header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
+import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link className={styles.logo} to="/" aria-label="My Portfolio">Logo</Link>
-        <Link to="/">Home</Link>
-        <Link to="/portfolio">Portfolio</Link>
-        <Link to="/contact">Contact me</Link>
+        <Link className={styles.logo} to="/" aria-label="My Portfolio"><Dogs/></Link>
+        <div className={styles.links}>
+          <Link to="/">Home</Link>
+          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/contact">Contact me</Link>
+        </div>
       </nav>
     </header>
   )
