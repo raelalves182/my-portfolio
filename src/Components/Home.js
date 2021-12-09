65b2109042d0f2284833bc815f5b71d2dae2130c
raelalves182 <ysrael.2009@hotmail.com>
2021-12-09 19:33:58 -0300

    feat(header): home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,9 +3,13 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Home
+      <span className="title container">Eu sou Israel Alves</span>
+
+      <div className='social-media'>
+        <a href="https://github.com/raelalves182"><i className='fab fa-github'></i></a>
+      </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
