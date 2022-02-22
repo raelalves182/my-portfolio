7eb3b9ee99632a3e6ef61ee22c7f7f072b597250
Israel Alves <ysrael.2009@hotmail.com>
2022-02-22 08:58:51 -0300

    feat(header): search bar

--- src/layouts/dashboard/Searchbar.js
+++ src/layouts/dashboard/Searchbar.js
@@ -1,79 +1,56 @@
 import { Icon } from '@iconify/react';
-import { useState } from 'react';
 import searchFill from '@iconify/icons-eva/search-fill';
 // material
-import { styled, alpha } from '@mui/material/styles';
-import { Box, Input, Slide, Button, InputAdornment, ClickAwayListener } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { Box, Input, Stack, Avatar, Typography } from '@mui/material';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 // components
 import { MIconButton } from '../../components/@material-extend';
 
 // ----------------------------------------------------------------------
 
 const APPBAR_MOBILE = 64;
-const APPBAR_DESKTOP = 92;
+const APPBAR_DESKTOP = 42;
 
 const SearchbarStyle = styled('div')(({ theme }) => ({
   top: 0,
   left: 0,
   zIndex: 99,
-  width: '100%',
+  marginRight: '32px',
   display: 'flex',
-  position: 'absolute',
   alignItems: 'center',
+  border: '1px solid #171137',
+  borderRadius: '5px',
   height: APPBAR_MOBILE,
-  backdropFilter: 'blur(6px)',
-  WebkitBackdropFilter: 'blur(6px)', // Fix on Mobile
   padding: theme.spacing(0, 3),
-  boxShadow: theme.customShadows.z8,
-  backgroundColor: `${alpha(theme.palette.background.default, 0.72)}`,
   [theme.breakpoints.up('md')]: {
     height: APPBAR_DESKTOP,
-    padding: theme.spacing(0, 5)
+    padding: theme.spacing(0, 1)
   }
 }));
 
 // ----------------------------------------------------------------------
 
 export default function Searchbar() {
-  const [isOpen, setOpen] = useState(false);
-
-  const handleOpen = () => {
-    setOpen((prev) => !prev);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
-    <ClickAwayListener onClickAway={handleClose}>
-      <div>
-        {!isOpen && (
-          <MIconButton onClick={handleOpen}>
-            <Icon icon={searchFill} width={20} height={20} />
-          </MIconButton>
-        )}
-
-        <Slide direction="down" in={isOpen} mountOnEnter unmountOnExit>
-          <SearchbarStyle>
-            <Input
-              autoFocus
-              fullWidth
-              disableUnderline
-              placeholder="Search…"
-              startAdornment={
-                <InputAdornment position="start">
-                  <Box component={Icon} icon={searchFill} sx={{ color: 'text.disabled', width: 20, height: 20 }} />
-                </InputAdornment>
-              }
-              sx={{ mr: 1, fontWeight: 'fontWeightBold' }}
-            />
-            <Button variant="contained" onClick={handleClose}>
-              Search
-            </Button>
-          </SearchbarStyle>
-        </Slide>
-      </div>
-    </ClickAwayListener>
+    <Box display="flex">
+      <SearchbarStyle>
+        <Input
+          autoFocus
+          fullWidth
+          disableUnderline
+          placeholder="Search for games…"
+          sx={{ mr: 1, fontWeight: 'fontWeightNormal', fontSize: '14px' }}
+        />
+        <MIconButton sx={{ borderLeft: '1px solid #171137', borderRadius: '0', color: '#ffffff', padding: '0 8px' }}>
+          <Icon icon={searchFill} width={20} height={20} />
+        </MIconButton>
+      </SearchbarStyle>
+      <Stack sx={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Avatar alt="Avatar" src="/static/aulus.png" />
+        <Typography margin={1.25}>Aulus</Typography>
+        <ArrowDropDownIcon />
+      </Stack>
+    </Box>
   );
 }
---
+++ src/layouts/main/NavbarLog.js
@@ -0,0 +1,99 @@
+import { Link as RouterLink, useLocation } from 'react-router-dom';
+// material
+import { styled } from '@mui/material/styles';
+import { Box, AppBar, Toolbar, Container } from '@mui/material';
+// Image
+import { ReactComponent as Beaten } from '../../assets/logo-beaten.svg';
+// hooks
+import useOffSetTop from '../../hooks/useOffSetTop';
+// components
+import { MHidden } from '../../components/@material-extend';
+//
+import MenuDesktop from './MenuDesktop';
+import MenuMobile from './MenuMobile';
+import navConfig from './MenuConfig';
+import Searchbar from '../dashboard/Searchbar';
+
+// ----------------------------------------------------------------------
+
+const APP_BAR_MOBILE = 64;
+const APP_BAR_DESKTOP = 80;
+
+const ToolbarStyle = styled(Toolbar)(({ theme }) => ({
+  height: APP_BAR_MOBILE,
+  transition: theme.transitions.create(['height', 'background-color'], {
+    easing: theme.transitions.easing.easeInOut,
+    duration: theme.transitions.duration.shorter
+  }),
+  [theme.breakpoints.up('md')]: {
+    height: APP_BAR_DESKTOP
+  }
+}));
+
+const ToolbarShadowStyle = styled('div')(({ theme }) => ({
+  left: 0,
+  right: 0,
+  bottom: 0,
+  height: 24,
+  zIndex: -1,
+  margin: 'auto',
+  borderRadius: '50%',
+  position: 'absolute',
+  width: `calc(100% - 48px)`,
+  boxShadow: theme.customShadows.none
+}));
+
+const BoxStyle = styled(Box)(({ theme }) => ({
+  boxShadow: theme.customShadows.none,
+  marginRight: 80
+}));
+
+// ----------------------------------------------------------------------
+
+export default function MainNavbar() {
+  const isOffset = useOffSetTop(100);
+  const { pathname } = useLocation();
+  const isHome = pathname === '/';
+
+  return (
+    <AppBar sx={{ boxShadow: 0, bgcolor: '#0B071B' }}>
+      <ToolbarStyle
+        disableGutters
+        sx={{
+          ...(isOffset && {
+            bgcolor: 'background.default',
+            height: { md: APP_BAR_DESKTOP - 16 }
+          })
+        }}
+      >
+        <Container
+          maxWidth="lg"
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}
+        >
+          <BoxStyle>
+            <RouterLink to="/">
+              <Beaten />
+            </RouterLink>
+          </BoxStyle>
+
+          <MHidden width="mdDown">
+            <MenuDesktop isOffset={isOffset} isHome={isHome} navConfig={navConfig} />
+          </MHidden>
+          <Box sx={{ flexGrow: 1 }} />
+
+          <Searchbar />
+
+          <MHidden width="mdUp">
+            <MenuMobile isOffset={isOffset} isHome={isHome} navConfig={navConfig} />
+          </MHidden>
+        </Container>
+      </ToolbarStyle>
+
+      {isOffset && <ToolbarShadowStyle />}
+    </AppBar>
+  );
+}
